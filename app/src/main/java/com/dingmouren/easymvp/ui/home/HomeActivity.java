bd8caaeeef3209bdf569661ecde3cb68041517f0
dingmouren <1341520108@qq.com>
2016-12-06 09:38:37 +0800

    HomeActivity中的视图展示使用fragment来展示，此处只是显示了图片

--- app/src/main/java/com/dingmouren/easymvp/ui/home/HomeActivity.java
+++ app/src/main/java/com/dingmouren/easymvp/ui/home/HomeActivity.java
@@ -1,9 +1,10 @@
 package com.dingmouren.easymvp.ui.home;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -27,138 +28,86 @@ import me.majiajie.pagerbottomtabstrip.Controller;
 import me.majiajie.pagerbottomtabstrip.PagerBottomTabLayout;
 import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectListener;
 
-public class HomeActivity extends BaseActivity implements HomeContract.View{
-    
-    @BindView(R.id.drawer_main)  DrawerLayout mDrawer;
-    @BindView(R.id.coordinator)  CoordinatorLayout mCoordinator;
-    @BindView(R.id.toolbar) Toolbar mToolbar;
-    @BindView(R.id.swipe_refresh)   SwipeRefreshLayout mSwipeRefresh;
-    @BindView(R.id.recycler) RecyclerView mRecycler;
-    @BindView(R.id.nav_view_main)  NavigationView mNavView;
-    @BindView(R.id.fab_main)  FloatingActionButton mFab;
-    @BindView(R.id.tab_bottom)  PagerBottomTabLayout mTabBottom;
+public class HomeActivity extends BaseActivity {
 
-    private GridLayoutManager mGridLayoutManager;
-    public HomeAdapter mHomeAdapter;
-    public HomePresenter mHomePresenter;
+    @BindView(R.id.drawer_main)
+    DrawerLayout mDrawer;
+    @BindView(R.id.coordinator)
+    CoordinatorLayout mCoordinator;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    RecyclerView mRecycler;
+    @BindView(R.id.nav_view_main)
+    NavigationView mNavView;
+    @BindView(R.id.fab_main)
+    FloatingActionButton mFab;
+    @BindView(R.id.tab_bottom)
+    PagerBottomTabLayout mTabBottom;
+
     private Controller mController;
     private ActionBarDrawerToggle mDrawerToggle;
-    
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+    private HomeFragment mHomeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
-        //初始化SwipeRefresh的颜色
-        setupSwipeRefresh();
         //初始化toolbar
         initToolbar();
         //初始化底部导航栏
         setupTabBottom();
-        //初始化布局
-        initView();
-        //初始化数据
-        initData();
+        //初始化首页视图
+        initHomeFragment();
+        mNavView.setNavigationItemSelectedListener(mNavgationViewItemSelectedListener);
     }
 
-    public void initToolbar(){
+    private void initHomeFragment() {
+        mFragmentManager = getFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mHomeFragment = new HomeFragment();
+        mFragmentTransaction.replace(R.id.frame_home_activity,mHomeFragment).commit();
+    }
+
+    public void initToolbar() {
         mToolbar.setTitle("");
         setSupportActionBar(mToolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawer,mToolbar,R.string.drawer_open,R.string.drawer_close);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
         mDrawerToggle.syncState();
     }
 
-    private void setupSwipeRefresh() {
-        if (mSwipeRefresh != null) {
-            mSwipeRefresh.setColorSchemeResources(R.color.blue,
-                    R.color.main_color, R.color.blue);
-            mSwipeRefresh.setProgressViewOffset(true, 0, (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics()));
-            mSwipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-                @Override
-                public void onRefresh() {
-                    mHomePresenter.requestData();
-                }
-            });
-        }
-    }
 
-    private void setupTabBottom(){
+    private void setupTabBottom() {
         mController = mTabBottom.builder()
-                .addTabItem(R.mipmap.home,"首页")
+                .addTabItem(R.mipmap.home, "首页")
                 .addTabItem(R.mipmap.music_icon, "Music")
                 .addTabItem(R.mipmap.weather, "天气")
                 .addTabItem(R.mipmap.setting, "设置")
                 .build();
         mController.addTabItemClickListener(mTabItemListener);
     }
-    private void initView() {
-        //列表相关
-        mGridLayoutManager = new GridLayoutManager(this,2);
-        mHomeAdapter = new HomeAdapter(this);
-        mRecycler.setHasFixedSize(true);
-        mRecycler.setLayoutManager(mGridLayoutManager);
-        mRecycler.setAdapter(mHomeAdapter);
 
-        //侧滑栏相关
-        mNavView.setNavigationItemSelectedListener(mNavgationViewItemSelectedListener);
-
-    }
-
-    private void initData(){
-        mHomePresenter = createPresenter();
-        setDataRefresh(true);
-        mHomePresenter.requestData();
-        mHomePresenter.addScrollListener();
-    }
-
-
-    public HomePresenter createPresenter(){
-        return new HomePresenter((HomeContract.View) this);
-    }
-
-
-    @Override
-    public void setDataRefresh(boolean refresh) {
-        if (refresh){
-            mSwipeRefresh.setRefreshing(refresh);
-        }else {
-            new Handler().postDelayed(()->mSwipeRefresh.setRefreshing(refresh),1000);//延时消失加载的loading
-        }
-    }
-
-    @Override
-    public GridLayoutManager getLayoutManager() {
-        return  mGridLayoutManager == null ? new GridLayoutManager( this,2 ) : mGridLayoutManager;
-    }
-
-    @Override
-    public RecyclerView getRecyclerView() {
-        return mRecycler;
-    }
-
-    @Override
-    public HomeAdapter getHomeAdapter() {
-        return mHomeAdapter == null ? new HomeAdapter(this) : mHomeAdapter;
-    }
 
     //底部导航栏item的监听
     OnTabItemSelectListener mTabItemListener = new OnTabItemSelectListener() {
         @Override
         public void onSelected(int index, Object tag) {
-            switch (index){
+            switch (index) {
                 case 0:
+
                     break;
                 case 1:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"开启音乐");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "开启音乐");
                     break;
                 case 2:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"开启天气");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "开启天气");
                     break;
                 case 3:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"开启设置");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "开启设置");
                     break;
                 default:
                     break;
@@ -167,18 +116,19 @@ public class HomeActivity extends BaseActivity implements HomeContract.View{
 
         @Override
         public void onRepeatClick(int index, Object tag) {
-            switch (index){
+            switch (index) {
                 case 0:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"相机被点击了");
+                    mHomeFragment = new HomeFragment();
+                    mFragmentTransaction.replace(R.id.frame_home_activity,mHomeFragment).commit();
                     break;
                 case 1:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"位置被点击了");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "位置被点击了");
                     break;
                 case 2:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"搜索被点击了");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "搜索被点击了");
                     break;
                 case 3:
-                    SnackbarUtils.showSimpleSnackbar(mDrawer,"帮助被点击了");
+                    SnackbarUtils.showSimpleSnackbar(mDrawer, "帮助被点击了");
                     break;
                 default:
                     break;
@@ -189,10 +139,10 @@ public class HomeActivity extends BaseActivity implements HomeContract.View{
     //侧滑栏选项的监听
     NavigationView.OnNavigationItemSelectedListener mNavgationViewItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
                 case R.id.item1:
-                    startActivity(new Intent(HomeActivity.this,GalleryActivity.class));
+                    startActivity(new Intent(HomeActivity.this, GalleryActivity.class));
                     break;
 
             }
---
+++ app/src/main/java/com/dingmouren/easymvp/ui/home/HomeFragment.java
@@ -0,0 +1,112 @@
+package com.dingmouren.easymvp.ui.home;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dingmouren.easymvp.R;
+import com.dingmouren.easymvp.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by dingmouren on 2016/12/6.
+ */
+
+public class HomeFragment extends BaseFragment implements HomeContract.View{
+
+    @BindView(R.id.swipe_refresh)  SwipeRefreshLayout mSwipeRefresh;
+    @BindView(R.id.recycler)  RecyclerView mRecycler;
+
+    private GridLayoutManager mGridLayoutManager;
+    public HomeAdapter mHomeAdapter;
+    public HomePresenter mHomePresenter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupSwipeRefresh();  //初始化SwipeRefresh的颜色
+        initView();//初始化视图
+        initData();//初始化数据
+    }
+
+    /**
+     * 初始化SwipeRefresh的颜色
+     */
+    private void setupSwipeRefresh() {
+        if (mSwipeRefresh != null){
+            mSwipeRefresh.setColorSchemeResources(R.color.main_color);//设置进度动画的颜色
+//            mSwipeRefresh.setProgressBackgroundColorSchemeResource(android.R.color.holo_blue_bright);//设置进度圈背景颜色
+            //这里进行单位换算  第一个参数是单位，第二个参数是单位数值，这里最终返回的是24dp对相应的px值
+            mSwipeRefresh.setProgressViewOffset(true,0, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,24,getResources().getDisplayMetrics()));
+            mSwipeRefresh.setOnRefreshListener(()-> mHomePresenter.requestData());
+        }
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView(){
+        //列表相关
+        mGridLayoutManager = new GridLayoutManager(getActivity(),2);
+        mHomeAdapter = new HomeAdapter(getActivity());
+        mRecycler.setHasFixedSize(true);
+        mRecycler.setLayoutManager(mGridLayoutManager);
+        mRecycler.setAdapter(mHomeAdapter);
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData(){
+        mHomePresenter = createPresenter();
+        setDataRefresh(true);
+        mHomePresenter.requestData();
+        mHomePresenter.addScrollListener();
+   }
+
+    /**
+     * 获取presenter实例
+     * @return
+     */
+    public HomePresenter createPresenter(){
+        return new HomePresenter((HomeContract.View) this);
+    }
+    @Override
+    public void setDataRefresh(boolean refresh) {
+        if (refresh){
+            mSwipeRefresh.setRefreshing(refresh);
+        }else {
+            new Handler().postDelayed(()->mSwipeRefresh.setRefreshing(refresh),1000);//延时消失加载的loading
+        }
+    }
+
+    @Override
+    public GridLayoutManager getLayoutManager() {
+        return  mGridLayoutManager == null ? new GridLayoutManager( getActivity(),2 ) : mGridLayoutManager;
+    }
+
+    @Override
+    public RecyclerView getRecyclerView() {
+        return mRecycler;
+    }
+
+    @Override
+    public HomeAdapter getHomeAdapter() {
+        return mHomeAdapter == null ? new HomeAdapter(getActivity()) : mHomeAdapter;
+    }
+}
