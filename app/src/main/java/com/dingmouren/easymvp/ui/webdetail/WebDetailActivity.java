4513f7cad9385b262757a882272bff9422b56289
dingmouren <1341520108@qq.com>
2016-12-15 08:11:40 +0800

    添加一些注释

--- app/src/main/java/com/dingmouren/easymvp/ui/webdetail/WebDetailActivity.java
+++ app/src/main/java/com/dingmouren/easymvp/ui/webdetail/WebDetailActivity.java
@@ -207,3 +207,87 @@ public class WebDetailActivity extends BaseActivity {
         super.onDestroy();
     }
 }
+//-------WebChromeClient-----------------------
+//        //获取Web页中的title用来设置自己界面中的title
+//        //当加载出错的时候，比如无网络，这时onReceiveTitle中获取的标题为 找不到该网页,
+//        //因此建议当触发onReceiveError时，不要使用获取到的title
+//        @Override
+//        public void onReceivedTitle(WebView view, String title) {
+//            MainActivity.this.setTitle(title);
+//        }
+//
+//        @Override
+//        public void onReceivedIcon(WebView view, Bitmap icon) {
+//            //
+//        }
+//
+//        @Override
+//        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+//            //
+//            return true;
+//        }
+//
+//        @Override
+//        public void onCloseWindow(WebView window) {
+//        }
+//
+//        //处理alert弹出框，html 弹框的一种方式
+//        @Override
+//        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+//            //
+//            return true;
+//        }
+//
+//        //处理confirm弹出框
+//        @Override
+//        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult
+//                result) {
+//            //
+//            return true;
+//        }
+//
+//        //处理prompt弹出框
+//        @Override
+//        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+//            //
+//            return true;
+//        }
+
+
+//-----------------WebViewClient-----------------------
+//        shouldOverrideUrlLoading(WebView view, String url)  最常用的，比如上面的。
+//        //在网页上的所有加载都经过这个方法,这个函数我们可以做很多操作。
+//        //比如获取url，查看url.contains(“add”)，进行添加操作
+//
+//        shouldOverrideKeyEvent(WebView view, KeyEvent event)
+//        //重写此方法才能够处理在浏览器中的按键事件。
+//
+//        onPageStarted(WebView view, String url, Bitmap favicon)
+//        //这个事件就是开始载入页面调用的，我们可以设定一个loading的页面，告诉用户程序在等待网络响应。
+//
+//        onPageFinished(WebView view, String url)
+//        //在页面加载结束时调用。同样道理，我们可以关闭loading 条，切换程序动作。
+//
+//        onLoadResource(WebView view, String url)
+//        // 在加载页面资源时会调用，每一个资源（比如图片）的加载都会调用一次。
+//
+//        onReceivedError(WebView view, int errorCode, String description, String failingUrl)
+//        // (报告错误信息)
+//
+//        doUpdateVisitedHistory(WebView view, String url, boolean isReload)
+//        //(更新历史记录)
+//
+//        onFormResubmission(WebView view, Message dontResend, Message resend)
+//        //(应用程序重新请求网页数据)
+//
+//        onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host,String realm)
+//        //（获取返回信息授权请求）
+//
+//        onReceivedSslError(WebView view, SslErrorHandler handler, SslError error)
+//        //重写此方法可以让webview处理https请求。
+//
+//        onScaleChanged(WebView view, float oldScale, float newScale)
+//        // (WebView发生改变时调用)
+//
+//        onUnhandledKeyEvent(WebView view, KeyEvent event)
+//        //（Key事件未被加载时调用）
