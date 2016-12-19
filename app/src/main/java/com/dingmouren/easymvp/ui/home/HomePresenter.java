9cbaaee886e3adb89ae95dc2d3804122b821c55f
dingmouren <1341520108@qq.com>
2016-12-19 17:32:32 +0800

    gif change

--- app/src/main/java/com/dingmouren/easymvp/ui/home/HomePresenter.java
+++ app/src/main/java/com/dingmouren/easymvp/ui/home/HomePresenter.java
@@ -39,7 +39,8 @@ public class HomePresenter implements HomeContract.Presenter {
             requestData();
         }
     };
-    public HomePresenter(HomeContract.View view){
+
+    public HomePresenter(HomeContract.View view) {
         this.mView = view;
         mRelativeProgressbar = mView.getProgressBarRelative();
         mProgressTextView = mView.getProgressBarTextView();
@@ -51,37 +52,39 @@ public class HomePresenter implements HomeContract.Presenter {
      * 请求数据，请求的是发布文章的日期集合，获取最新的日期
      */
     @Override
-    public void requestData(){
+    public void requestData() {
         mView.setDataRefresh(true);
         ApiManager.getApiInstance().mApiService.getGankDatePushed()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(listGankResult -> getData(listGankResult.results.get(0)),this :: loadError);
+                .subscribe(listGankResult -> getData(listGankResult.results.get(0)), this::loadError);
     }
 
     /**
      * 异常处理
+     *
      * @param throwable
      */
     @Override
-    public void loadError(Throwable throwable){
+    public void loadError(Throwable throwable) {
         throwable.printStackTrace();
-        new Handler().postDelayed(()->{
+        new Handler().postDelayed(() -> {
             mRelativeProgressbar.setVisibility(View.VISIBLE);
             mProgressbar.setIndeterminateDrawable(mProgressbar.getContext().getResources().getDrawable(R.mipmap.loading_error));
             mProgressbar.setProgressDrawable(mProgressbar.getContext().getResources().getDrawable(R.mipmap.loading_error));
             mProgressTextView.setText("这里空空如也，点击刷新哟~~");
-        },3000);
+        }, 3000);
 
     }
 
     /**
      * 对获取的最新日期进行解析，去请求这个日期的数据
+     *
      * @param date
      */
-    private void getData(String date){
-        mDate =date.replace('-', '/');
-        JLog.e("mDate------",mDate);
+    private void getData(String date) {
+        mDate = date.replace('-', '/');
+        JLog.e("mDate------", mDate);
         if (mDate != null) {
             ApiManager.getApiInstance().mApiService.getGankDay(mDate)
                     .subscribeOn(Schedulers.io())
@@ -93,14 +96,17 @@ public class HomePresenter implements HomeContract.Presenter {
     private void parseData(GankResultCategory results) {
         mList = new ArrayList<>();
         mList.clear();
-        mList.addAll(results.getAndroid());
-        mList.addAll(results.getiOS());
-        if (null != results.get前端()) {
+        if (null != results.getAndroid())
+            mList.addAll(results.getAndroid());
+        if (null != results.getiOS())
+            mList.addAll(results.getiOS());
+        if (null != results.get前端())
             mList.addAll(results.get前端());
-        }
-        mList.addAll(results.get拓展资源());
-        mList.addAll(results.get休息视频());
-        mView.setData(mList,results.get福利().get(0).getUrl());
+        if (null != results.get拓展资源())
+            mList.addAll(results.get拓展资源());
+        if (null != results.get休息视频())
+            mList.addAll(results.get休息视频());
+        mView.setData(mList, results.get福利().get(0).getUrl());
         mView.setDataRefresh(false);
 
     }
