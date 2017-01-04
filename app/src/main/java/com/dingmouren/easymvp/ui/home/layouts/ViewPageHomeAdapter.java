edf590287c8f22a3cc8c01e434388328fc3c9b52
dingmouren <1341520108@qq.com>
2017-01-04 18:39:15 +0800

    修改UI

--- app/src/main/java/com/dingmouren/easymvp/ui/home/layouts/ViewPageHomeAdapter.java
+++ app/src/main/java/com/dingmouren/easymvp/ui/home/layouts/ViewPageHomeAdapter.java
@@ -26,8 +26,7 @@ public class ViewPageHomeAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         ImageView imgView = new ImageView(container.getContext());
-//        MyGlideImageLoader.displayImage(mListImgUrls.get(position),imgView);
-        Glide.with(container.getContext()).load(mListImgUrls.get(position)).into(imgView);
+        MyGlideImageLoader.displayImage(mListImgUrls.get(position),imgView);
         imgView.setOnClickListener((view) ->Glide.with(container.getContext()).load(mListImgUrls.get(position)).into(imgView));
         container.addView(imgView);
         return imgView;
