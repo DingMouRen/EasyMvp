f301555d9e113516802f3f75a323b364f60e030f
dingmouren <1341520108@qq.com>
2017-02-03 08:55:44 +0800

    去除部分无用代码

--- app/src/main/java/com/dingmouren/easymvp/bean/video/VideoResult.java
+++ app/src/main/java/com/dingmouren/easymvp/bean/video/VideoResult.java
@@ -18,24 +18,6 @@ public class VideoResult<T> {
     private String showapi_res_error;
     public T showapi_res_body;
 
-    public int getShowapi_res_code() {
-        return showapi_res_code;
-    }
-
-    public void setShowapi_res_code(int showapi_res_code) {
-        this.showapi_res_code = showapi_res_code;
-    }
-
-    public String getShowapi_res_error() {
-        return showapi_res_error;
-    }
-
-    public void setShowapi_res_error(String showapi_res_error) {
-        this.showapi_res_error = showapi_res_error;
-    }
-
-
-
     public static class ShowapiResBodyBean<T> {
         /**
          * ret_code : 0
@@ -44,16 +26,6 @@ public class VideoResult<T> {
 
         private int ret_code;
         public T pagebean;
-
-        public int getRet_code() {
-            return ret_code;
-        }
-
-        public void setRet_code(int ret_code) {
-            this.ret_code = ret_code;
-        }
-
-
 
         public static class PagebeanBean<T> {
             /**
@@ -69,41 +41,6 @@ public class VideoResult<T> {
             private int allNum;
             private int maxResult;
             public T contentlist;
-
-            public int getAllPages() {
-                return allPages;
-            }
-
-            public void setAllPages(int allPages) {
-                this.allPages = allPages;
-            }
-
-            public int getCurrentPage() {
-                return currentPage;
-            }
-
-            public void setCurrentPage(int currentPage) {
-                this.currentPage = currentPage;
-            }
-
-            public int getAllNum() {
-                return allNum;
-            }
-
-            public void setAllNum(int allNum) {
-                this.allNum = allNum;
-            }
-
-            public int getMaxResult() {
-                return maxResult;
-            }
-
-            public void setMaxResult(int maxResult) {
-                this.maxResult = maxResult;
-            }
-
-
-
         }
     }
 }
