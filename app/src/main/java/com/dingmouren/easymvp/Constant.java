8991610a6207124ab1b0e865252c3052fbb8d4fc
dingmouren <1341520108@qq.com>
2017-02-03 08:13:03 +0800

    去除部分无用代码

--- app/src/main/java/com/dingmouren/easymvp/Constant.java
+++ app/src/main/java/com/dingmouren/easymvp/Constant.java
@@ -8,7 +8,6 @@ public class Constant {
     //showapi的appid和密钥
     public static final String SHOW_API_APP_ID = "30536";
     public static final String SHOW_API_SIGN ="afb5409eac144403a8f13c6bb51cac1f";
-    public static final String TYPE = "41";//表示是视频
     //BaseURL相关
     public static final String GANK_BASE_URL = "http://gank.io/api/";
     public static final String VIDEO_BASE_URL ="http://route.showapi.com/";
